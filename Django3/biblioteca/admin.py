c52b2155febcdf6fc881b6efe65705f74f7d4f98
Lucio <LucioMorales>
2020-08-19 17:08:40 -0300

    Creacion de Models y registros en el Admin

--- Django3/biblioteca/admin.py
+++ Django3/biblioteca/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Prestamo)
+admin.site.register(Persona)
+admin.site.register(Profesor)
+admin.site.register(Alumno)
+admin.site.register(Material)
+admin.site.register(Libro)
+admin.site.register(Revista)
